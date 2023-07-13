f8df8c59dcd3d3af9331b0f74a5005d7fb8281d0
marcoccari <marcutox2000@gmail.com>
2023-07-13 17:41:48 -0500

    base de datos

---
+++ database/migrations/2023_07_08_222726_create_estudiantes_cursos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estudiantes_cursos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('estudiante_id');
+            $table->foreign('estudiante_id')->references('id')->on('estudiantes');
+            $table->unsignedBigInteger('docente_id');
+            $table->foreign('docente_id')->references('id')->on('docentes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estudiantes_cursos');
+    }
+};
